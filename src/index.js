feaf415a6f622f15fcaf1dbc00c860b9b8478aa5
annie1004619 <annie1004619@naver.com>
2021-07-09 06:54:39 +0900

    fix: sentry  연동

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -9,6 +11,16 @@ import { PersistGate } from 'redux-persist/integration/react';
 import configStore from './config/store';
 
 const { store, persistor } = configStore();
+
+Sentry.init({
+    dsn: "https://4b09c42b72be4125b4995342b0775a49@o915536.ingest.sentry.io/5855919",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
     <Provider store={store}>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Header from "../components/Header/index"
 
 function Landing() {
+
+    const onClick = (good) => {
+        return good.map(x => x * 2)
+    }
+
     return (
         <div>
             <Header />
             홈
+            <button onClick={e=>onClick()}>버튼</button>
         </div>
     );
 }
